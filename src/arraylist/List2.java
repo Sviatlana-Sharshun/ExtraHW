d67cae7c47c2c9e2760dd50ae7a440ebbdc68c1f
Sviatlana Sharshun <ssharshun@web3tech.ru>
2022-03-19 02:26:48 +0300

    ArrayList

---
+++ src/arraylist/List2.java
@@ -0,0 +1,19 @@
+package arraylist;
+
+import java.util.ArrayList;
+public class List2 {
+
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Акакий"));
+        students.add(new Student("Любослав"));
+
+        printStudentNames(students);
+    }
+
+    public static void printStudentNames(ArrayList students) {
+//    for (int i = 0; i < students.size(); i++) {
+//      System.out.println(students.get(i).getName());
+//    }
+    }
+}
